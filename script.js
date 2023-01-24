cdc1f4467b70b4ac689b2e6d02bfb3d3e78a3da9
ATUL WAMAN NAGOSE <44936500+atulnagose1499@users.noreply.github.com>
2023-01-24 13:40:09 +0530

    web

---
+++ script.js
@@ -0,0 +1,12 @@
+// var tablinks = document.getElementById("tab-links");
+// var tabcontents = document.getElementById("tab-contents");
+
+// function opentab(tabname) {
+//   for (tablink of tablinks) {
+//     tablink.classList.remove("active-link");
+//   }
+//   for (tabcontent of tabcontents) {
+//     tabcontent.classList.remove("active-tab");
+//   }
+// }
+
