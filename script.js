49cd289cd35c24cb09e5023c0912c9167b1ed918
ATUL WAMAN NAGOSE <44936500+atulnagose1499@users.noreply.github.com>
2023-01-24 17:19:08 +0530

    add

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-// var tablinks = document.getElementById("tab-links");
-// var tabcontents = document.getElementById("tab-contents");
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
 
-// function opentab(tabname) {
-//   for (tablink of tablinks) {
-//     tablink.classList.remove("active-link");
-//   }
-//   for (tabcontent of tabcontents) {
-//     tabcontent.classList.remove("active-tab");
-//   }
-// }
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
 
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+  sidemenu.style.right = "0";
+}
+function closemenu() {
+  sidemenu.style.right = "-200px";
+}
